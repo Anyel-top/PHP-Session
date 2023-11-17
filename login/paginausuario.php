7d0ca9484f6f92200dafac47c50a4f7a05ff711d
Angel Paul Patiño Diaz <146494692+Anyel-top@users.noreply.github.com>
2023-11-17 09:00:31 -0500

    update project

--- login/paginausuario.php
+++ login/paginausuario.php
@@ -3,8 +3,17 @@ session_start();
 $id = $_SESSION['id'];
 $nombre = $_SESSION['usuario'];
 
+$conn = new mysqli(SEVERMAME, USERNAME, PASSWORD, DBNAME);
 
+// generate query 
+$sql = "SELECT ciudad from usuario where IDUsuario = $id";
+
+$resultado = mysqli_query($conn, $sql) or die(mysqli_error($conn));
+
+$row = mysqli_fetch_array($resultado);
+$ciudad = $row['ciudad'];
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -13,6 +22,7 @@ $nombre = $_SESSION['usuario'];
     <title>Pagina de usuario</title>
 </head>
 <body>
-<h1>Bienvenido, <?php echo $nombre; ?>!</h1>
+    <h1>Bienvenido, <?php echo $nombre . ' ' . $ciudad; ?>!</h1>
+    <a href="cerrar.php">Cerrar Sesion</a>
 </body>
 </html>
